858c1cf47d07a876da99faec1391c736663d1774
Jose Melendez <JoseM_Melendez@icloud.com>
2017-09-06 11:33:37 -0400

    Added project and customized the main.storyboard

---
+++ ColorWizard/ColorWizard/ViewControllers/ColorPickerVC.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPickerVC.swift
+//  ColorWizard
+//
+//  Created by Jose Melendez on 9/6/17.
+//  Copyright © 2017 JoseMelendez. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ ColorWizard/ColorWizard/ViewControllers/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ColorWizard
+//
+//  Created by Jose Melendez on 9/6/17.
+//  Copyright © 2017 JoseMelendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorNameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
+
